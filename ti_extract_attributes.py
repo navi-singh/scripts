9582fff71263492d95aff751db5ce94d89c5d63d
navi-singh <33882768+navi-singh@users.noreply.github.com>
2024-04-20 12:14:38 -0500

    Update ti_extract_attributes.py
    Updated header

--- ti_extract_attributes.py
+++ ti_extract_attributes.py
@@ -8,14 +8,30 @@ import requests
 #     data = json.load(file)
 
 url = 'https://www.tesla.com/inventory/api/v4/inventory-results?query={"query":{"model":"my","condition":"used","options":{"TRIM":["LRAWD"],"WHEELS":["NINETEEN"],"VehicleHistory":["CLEAN"],"Year":["2023"]},"arrangeby":"Price","order":"asc","market":"US","language":"en","super_region":"north america","lng":-96.0996978,"lat":41.28991329999999,"zip":"68164","range":0,"region":"NE"},"offset":50,"count":50,"outsideOffset":0,"outsideSearch":true,"isFalconDeliverySelectionEnabled":false,"version":null}'
+# headers = {
+#     'authority': 'www.tesla.com',
+#     'accept': '*/*',
+#     'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,tr-TR;q=0.6,tr;q=0.5',
+#     'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+#     'sec-ch-ua-mobile': '?0',
+#     'sec-ch-ua-platform': '"macOS"',
+#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+# }
 headers = {
-    'authority': 'www.tesla.com',
-    'accept': '*/*',
-    'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7,tr-TR;q=0.6,tr;q=0.5',
-    'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+'Cache-Control': 'max-age=0',
+'Sec-Ch-Ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+'Sec-Ch-Ua-Mobile': '?0',
+'Sec-Ch-Ua-Platform': '"Windows"',
+'Sec-Fetch-Dest': 'document',
+'Sec-Fetch-Mode': 'navigate',
+'Sec-Fetch-Site': 'none',
+'Sec-Fetch-User': '?1',
+'Upgrade-Insecure-Requests': '1',
+'Sec-Fetch-User': '?1'
 }
 
 resp = requests.get(url, headers=headers, timeout=5)
